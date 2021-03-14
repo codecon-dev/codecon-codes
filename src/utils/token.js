d342050f0caf7231f5e3299226183c66f54c531f
Mark Kop <marcelokopmann@gmail.com>
2021-03-14 12:54:42 -0300

    lint: token utils

--- src/utils/token.js
+++ src/utils/token.js
@@ -4,7 +4,7 @@ import { openFile, saveFile } from './files'
  * @typedef UserClaim
  * @property {string} username
  * @property {string} email
- * @property {ISODateString} claimedAt
+ * @property {string} claimedAt ISO Date String.
  */
 
 /**
@@ -16,81 +16,85 @@ import { openFile, saveFile } from './files'
  * @property {number} totalClaims
  * @property {number} remainingClaims
  * @property {UserClaim[]} claimedBy
- * @property {ISODateString} createdBy
- * @property {ISODateString} createdAt
- * @property {ISODateString} expireAt
+ * @property {string} createdBy ISO Date String.
+ * @property {string} createdAt ISO Date String.
+ * @property {string} expireAt ISO Date String.
  */
 
-
 /**
- * Get all tokens from database
+ * Get all tokens from database.
+ *
  * @returns {Token[]}
  */
-export async function getDatabaseTokens() {
-    try {
-        const tokens = openFile('data/tokens.json')
-        return tokens
-    } catch (error) {
-        console.log(error)
-    }
+export async function getDatabaseTokens () {
+  try {
+    const tokens = openFile('data/tokens.json')
+    return tokens
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 /**
- * Get a token from database by its code
+ * Get a token from database by its code.
+ *
+ * @param {Token}code
  * @returns {Token}
  */
-export async function getDatabaseTokenByCode(code) {
-    try {
-        const tokens = openFile('data/tokens.json')
-        return tokens.find(token => token.code === code)  
-    } catch (error) {
-        console.log(error)
-    }
+export async function getDatabaseTokenByCode (code) {
+  try {
+    const tokens = openFile('data/tokens.json')
+    return tokens.find(token => token.code === code)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 /**
- * Creates a new token
- * @param {Token} token 
+ * Creates a new token.
+ *
+ * @param {Token} token
  * @returns {Token[]}
  */
-export async function createDatabaseToken(token) {
-    try {
-        const { code } = token
-        const tokens = openFile('data/tokens.json')
-        const existingToken = tokens.find(token => token.code === code)
-        if (existingToken) {
-            throw new Error(`Error on Token Creation: Token ${code} already exists`)
-        }
-        tokens.push(token)
-        saveFile('data/tokens.json', tokens)
-        return tokens
-    } catch (error) {
-        console.log(error)
+export async function createDatabaseToken (token) {
+  try {
+    const { code } = token
+    const tokens = openFile('data/tokens.json')
+    const existingToken = tokens.find(token => token.code === code)
+    if (existingToken) {
+      throw new Error(`Error on Token Creation: Token ${code} already exists`)
     }
+    tokens.push(token)
+    saveFile('data/tokens.json', tokens)
+    return tokens
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 /**
- * Update a token
- * @param {Token} token 
+ * Update a token.
+ *
+ * @param {Token} token
  * @returns {Token}
  */
-export async function updateDatabaseToken(token) {
-    try {
-        const { code } = token
-        const tokens = openFile('data/tokens.json')
-        const existingToken = tokens.find(token => token.code === code)
-        if (!existingToken) {
-            throw new Error(`Error on Token Update: Token ${code} was not found`)
-        }
-        const updatedToken = {
-            ...existingToken,
-            ...token
-        }
-        const existingTokenIndex = tokens.indexOf(existingToken)
-        tokens[existingTokenIndex] = updatedToken
-        saveFile('data/tokens.json', tokens)
-        return updatedToken
-    } catch (error) {
-        console.log(error)
+export async function updateDatabaseToken (token) {
+  try {
+    const { code } = token
+    const tokens = openFile('data/tokens.json')
+    const existingToken = tokens.find(token => token.code === code)
+    if (!existingToken) {
+      throw new Error(`Error on Token Update: Token ${code} was not found`)
     }
+    const updatedToken = {
+      ...existingToken,
+      ...token
+    }
+    const existingTokenIndex = tokens.indexOf(existingToken)
+    tokens[existingTokenIndex] = updatedToken
+    saveFile('data/tokens.json', tokens)
+    return updatedToken
+  } catch (error) {
+    console.log(error)
+  }
 }
